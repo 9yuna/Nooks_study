605c3da4780b40f72ed3514136e8e845f805a265
Gyuna Bae <9yuna.b@gmail.com>
2021-10-31 20:49:45 +0900

    useTabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./styles.css";
-import useInput from "./useInput";
+import useTabs from "./useTabs";
+
+const content = [
+  { tab: "Section 1", content: "I'm the content of the Section 1" },
+  { tab: "Section 2", content: "I'm the content of the Section 2" },
+];
 
 const App = () => {
-  const maxLen = (value) => value.length < 10;
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <input placeholder={"Name"} {...name} />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      {currentItem.content}
     </div>
   );
 };
