a6d90634c4542b82963bb01d8086113914d23d9f
Gyuna Bae <9yuna.b@gmail.com>
2021-10-31 22:13:30 +0900

    useConfirm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./styles.css";
-import useClick from "./useClick";
+import useConfirm from "./useConfirm";
 
 const App = () => {
-  const sayHello = () => console.log("Hello");
-  const title = useClick(sayHello);
+  const deleteWorld = () => console.log("Deleting the world...");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
   return (
     <div className="App">
-      <h1 ref={title}>Hi</h1>
+      <button onClick={confirmDelete}>Delete the world</button>
     </div>
   );
 };
